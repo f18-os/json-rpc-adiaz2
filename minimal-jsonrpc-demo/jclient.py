fe5ffc395fada794e4a8765edd8f56cc678b4707
Alejandro Diaz <alejandrodiaz@Alejandros-MacBook-Pro.local>
2018-11-12 17:41:11 -0700

    created function to convert python graph object from a graph to json and back to a graph so that it can be transferred

--- minimal-jsonrpc-demo/jclient.py
+++ minimal-jsonrpc-demo/jclient.py
@@ -2,6 +2,7 @@
 # https://github.com/seprich/py-bson-rpc/blob/master/README.md#quickstart
 
 import socket
+from node import *
 from bsonrpc import JSONRpc
 from bsonrpc.exceptions import FramingError
 from bsonrpc.framing import (
@@ -9,13 +10,37 @@ from bsonrpc.framing import (
 
 # Cut-the-corners TCP Client:
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('localhost', 50001))
+s.connect(('localhost', 50003))
 
 rpc = JSONRpc(s,framing_cls=JSONFramingNone)
 server = rpc.get_peer_proxy()
+
 # Execute in server:
-result = server.swapper('Hello World!')
+leaf1 = node("leaf1")
+leaf2 = node("leaf2")
+
+root = node("root", [leaf1, leaf1, leaf2])
+
+print("graph before increment")
+root.show()
+
+def graphToJSON(graph):
+    jsonString = 'name: ' + graph.name + '\n'
+    jsonString += 'val: ' + str(graph.val) + '\n'
+    jsonString += 'children: {\n'
+    for c in graph.children:
+         jsonString += graphToJSON(c)
+         jsonString += ','
+    jsonString += '}'
+    return jsonString
+
+result = server.increment(graphToJSON(root))
+
+print("graph after increment")
+root.show()
+
 # "!dlroW olleH"
+
 print(result)
 
 print(server.nop({1:[2,3]}))
--- minimal-jsonrpc-demo/jserver.py
+++ minimal-jsonrpc-demo/jserver.py
@@ -1,13 +1,10 @@
-# minimalistic server example from 
-# https://github.com/seprich/py-bson-rpc/blob/master/README.md#quickstart
-
 import socket
 from bsonrpc import JSONRpc
+from node import *
 from bsonrpc import request, service_class
 from bsonrpc.exceptions import FramingError
 from bsonrpc.framing import (
 	JSONFramingNetstring, JSONFramingNone, JSONFramingRFC7464)
-
 
 # Class providing functions for the client to use:
 @service_class
@@ -21,13 +18,34 @@ class ServerServices(object):
   def nop(self, txt):
     print(txt)
     return txt
+  
+  @request
+  def increment(self, graph):
+    graph = JSONToGraph(graph)
+    print('\n\n this is the type of the graph ' type(graph))
+    graph.val += 1;
+    for c in graph.children:
+        self.increment(c)
+
+  def JSONToGraph(self, jsonString):
+    jsonArr = jsonString.split('\n')
+    name = jsonArr[0].split(':')[1]
+    val = jsonArr[1].split(':')[1]
+    children = []
+    for c in jsonArr[3].split(','):
+      children.append(self.JSONToGraph(c))
+    graph = node(name, children)
+    print('\n\n this is the type of the graph2312 ' +  type(graph))
+    graph.val = val
+    return graph
 
 # Quick-and-dirty TCP Server:
 ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ss.bind(('localhost', 50001))
+ss.bind(('localhost', 50003))
 ss.listen(10)
 
 while True:
   s, _ = ss.accept()
   # JSONRpc object spawns internal thread to serve the connection.
   JSONRpc(s, ServerServices(),framing_cls=JSONFramingNone)
+
